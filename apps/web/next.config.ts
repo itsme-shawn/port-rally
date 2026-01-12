a2fea40b4ecd3a1bb6f54218941aab54266d6cc5
HyeonSoo Choi (shawn) <48885608+itsme-shawn@users.noreply.github.com>
2026-01-13 04:57:08 +0900

    [chore] 프로젝트 환경 설정 및 의존성 업데이트
    - API 서버 도메인 설정 및 필요한 라이브러리 설치
    
    - 환경 변수(API_SERVER_DOMAIN) 설정 반영
    
    - @tanstack/react-query 등 신규 라이브러리 추가

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    API_SERVER_DOMAIN: process.env.API_SERVER_DOMAIN,
+  },
 };
 
 export default nextConfig;
